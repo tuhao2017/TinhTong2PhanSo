d9b4b2577682502d0d95f4bb1055a544a716d973
NATIVE <NATIVE@DESKTOP-UK01LRC>
2018-01-26 18:32:41 +0700

    Add project files.

---
+++ TinhTong2PhanSo/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace TinhTong2PhanSo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhanSo1 = new System.Windows.Forms.TextBox();
+            this.txtPhanSo2 = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phân Số 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phân Số 2";
+            // 
+            // txtPhanSo1
+            // 
+            this.txtPhanSo1.Location = new System.Drawing.Point(107, 42);
+            this.txtPhanSo1.Name = "txtPhanSo1";
+            this.txtPhanSo1.Size = new System.Drawing.Size(100, 20);
+            this.txtPhanSo1.TabIndex = 2;
+            // 
+            // txtPhanSo2
+            // 
+            this.txtPhanSo2.Location = new System.Drawing.Point(107, 65);
+            this.txtPhanSo2.Name = "txtPhanSo2";
+            this.txtPhanSo2.Size = new System.Drawing.Size(100, 20);
+            this.txtPhanSo2.TabIndex = 3;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(34, 131);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(173, 23);
+            this.btnCong.TabIndex = 4;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Location = new System.Drawing.Point(107, 112);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(46, 13);
+            this.lblKetQua.TabIndex = 5;
+            this.lblKetQua.Text = "Kết Quả";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 261);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.txtPhanSo2);
+            this.Controls.Add(this.txtPhanSo1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPhanSo1;
+        private System.Windows.Forms.TextBox txtPhanSo2;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
+
---
+++ TinhTong2PhanSo/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TinhTong2PhanSo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //public PhanSo ps;
+        /*
+         * Struct Phân Số
+         */
+        
+        struct PhanSo
+        {
+            public int tu;
+            public int mau;
+        }
+
+        PhanSo ps1;
+        PhanSo ps2;
+        
+        /*
+         * Hàm Nhập
+         */
+
+        private void Nhap(ref PhanSo ps1, ref PhanSo ps2)
+        {
+            // phan so 1
+
+            string phanso1 = txtPhanSo1.Text;
+
+            string[] tam1 = phanso1.Split("/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            if (tam1.Length > 1)
+            {
+                ps1.tu = int.Parse(tam1[0]);
+                ps1.mau = int.Parse(tam1[1]);
+            }
+
+            //phan so 2
+
+            string phanso2 = txtPhanSo2.Text;
+
+            string[] tam2 = phanso2.Split("/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            ps2.tu = int.Parse(tam2[0]);
+
+            ps2.mau = int.Parse(tam2[1]);
+        }
+
+        /*
+         * Tính tổng
+         */
+        private PhanSo Cong(PhanSo ps1, PhanSo ps2)
+        {
+
+            PhanSo ketqua;
+
+            ketqua.tu = (ps1.tu * ps2.mau) + (ps1.mau * ps2.tu);
+
+            ketqua.mau = ps1.mau * ps2.mau;
+
+            PhanSo kqrutgon =  RutGon(ketqua); 
+
+            return kqrutgon;
+        }
+        
+        /*
+         * HAM UOC SO CHUNG
+         */
+        private int USChung(int a, int b)
+        {
+            int ketqua = 0;
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            ketqua = a; 
+            return ketqua;
+        }
+
+        /*
+         * Hàm rút gon
+         */
+
+        private PhanSo RutGon(PhanSo ps1)
+        {
+            PhanSo ketqua;
+
+            int uchung = USChung(ps1.tu,ps1.mau);
+
+            ketqua.tu = ps1.tu /uchung;
+            ketqua.mau = ps1.mau / uchung;
+
+            return ketqua;
+        }
+
+        /*
+         * Hàm xuất chuỗi
+         */
+
+         private string Xuat(PhanSo ps)
+        {
+            if(ps.tu == ps.mau)
+            {
+                return "1";
+            }
+            else
+            {
+                return ps.tu + "/" + ps.mau;
+            }
+        }
+
+        /*
+         * Ham tach chuoi
+         */
+
+        private void TachChuoi(ref PhanSo ps1, ref PhanSo ps2)
+        {
+            //phan so 1
+
+            string[] a = { txtPhanSo1.Text };
+
+            int i = 0;
+            while (a[i] != "\0")
+            {
+                ps1.tu = int.Parse(a[i]);
+                ps1.mau = int.Parse(a[i]);
+                i++;
+            }
+        
+            //phan so 2
+
+        }
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            Nhap(ref ps1,ref ps2);
+
+            PhanSo ketqua;
+
+            ketqua = Cong(ps1, ps2);
+
+            string final;
+
+            final = Xuat(ketqua);
+
+            lblKetQua.Text = final;
+        }
+    }
+}
